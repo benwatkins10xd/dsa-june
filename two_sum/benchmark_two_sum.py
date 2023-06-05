72777a5f40d848064519d6ce06e4a1d9ade44fb4
Ben Watkins <benwatkins01@hotmail.com>
2023-06-05 20:11:50 +0100

    added optimisations to code

--- two_sum/benchmark_two_sum.py
+++ two_sum/benchmark_two_sum.py
@@ -1,36 +1,63 @@
+import timeit
 import time
+import gc
+import os
 import random
 import matplotlib.pyplot as plt
 
 times = []
 
-def two_sum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    return None
+'''
+This will benchmark the solutions to two sum.
 
-def generate_large_list(size):
-    return [random.randint(-1000, 1000) for _ in range(size)]
+Timing in python can give results with high variations. This is due to:
+- garbage collection
+- other processes using resources
+- processes taking priority
 
-def time_two_sum(array_sizes):
-    for size in array_sizes:
-        nums = generate_large_list(size)
-        target = random.randint(-2000, 2000)
+So we'll disable GC and set process priority to high below.
+'''
 
-        start_time = time.perf_counter()
-        print(start_time)
-        two_sum(nums, target)
-        end_time = time.perf_counter()
+# disable garbage collection
+gc.disable()
 
-        time_taken = end_time - start_time
-        times.append(time_taken)
-        print(f"Array Size: {size}, Time Taken: {time_taken} seconds")
+# set process priority to high
+if os.name == 'nt':
+    import psutil
+    psutil.Process(os.getpid()).nice(psutil.HIGH_PRIORITY_CLASS)
 
-# Example usage:
-array_sizes = [100000, 1000000, 10000000]
-time_two_sum(array_sizes)
+class Solution:
+    def naive_two_sum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return None
 
-plt.plot(array_sizes, times)
-plt.show()
+# Benchmark function
+def benchmark_two_sum(array_size):
+    nums = [random.randint(1, 100) for _ in range(array_size)]
+    target = random.randint(1, 200)
+    
+    def wrapper():
+        sol = Solution()
+        # change this for naive/optimised
+        sol.naive_two_sum(nums, target)
+    
+    duration = timeit.timeit(wrapper, number=10000)  # Perform 10,000 iterations
+    times.append(duration)
+    print(f"Benchmark for array size {array_size}: {duration:.6f} seconds")
+
+# Test different array sizes
+array_sizes = [10, 100, 1000, 3000, 6000, 8000, 10000]
+
+for size in array_sizes:
+    benchmark_two_sum(size)
+
+plt.plot(array_sizes, times, "+", markersize=8)
+plt.xlabel("Array size")
+plt.ylabel("Time (seconds)")
+plt.savefig(f'{time.time()}.png')
+print(f"Created {time.time()}.png")
+
+
